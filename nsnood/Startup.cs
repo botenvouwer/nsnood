bcc8261d2367236adf3bcf79445c2be7d98ccf1b
sebastiaan1997 <sebastiaan.saarloos@gmail.com>
2018-11-23 17:02:28 +0100

    ABC

--- nsnood/Startup.cs
+++ nsnood/Startup.cs
@@ -27,8 +27,8 @@ namespace nsnood
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseCors(builder =>
-                builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            /**app.UseCors(builder =>
+                builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());**/
             app.Use((context, next) =>
             {
                 context.Response.Headers["Access-Control-Allow-Origin"] = "*";
