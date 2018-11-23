1c34b931055d2a13afa2cfdbec89a518f2d5627f
sebastiaan1997 <sebastiaan.saarloos@gmail.com>
2018-11-23 16:35:34 +0100

    Added corse fix

--- nsnood/Startup.cs
+++ nsnood/Startup.cs
@@ -27,6 +27,7 @@ namespace nsnood
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
