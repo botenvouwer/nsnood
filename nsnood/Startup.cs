3708b78cee577bc7af04ee0faf59fc0a2b141e14
sebastiaan1997 <sebastiaan.saarloos@gmail.com>
2018-11-23 16:40:51 +0100

    *

--- nsnood/Startup.cs
+++ nsnood/Startup.cs
@@ -27,7 +27,8 @@ namespace nsnood
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseCors();
+            app.UseCors(builder =>
+                builder.WithOrigins("*"));
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
